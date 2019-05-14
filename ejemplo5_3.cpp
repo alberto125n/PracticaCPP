f954b6ca5ae3f5b3fcc1fa2cad4ddbdebab872db
alberto125n <45520264+alberto125n@users.noreply.github.com>
2019-05-15 01:03:38 +0200

    Create ejemplo5_3.cpp

---
+++ ejemplo5_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char car;
+   bool digito_leido = false;
+   while (!digito_leido)
+   {
+       cout << "Introduzca un caracter:"; 
+       cin >> car;
+       digito_leido = (('0'<=car)&&(car<='9'));
+   }
+   return 0;
+}
