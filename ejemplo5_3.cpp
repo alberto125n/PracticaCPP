efda40a8e23b93018567a4940e857f71c4f84191
alberto125n <45520264+alberto125n@users.noreply.github.com>
2019-05-15 01:06:26 +0200

    Update ejemplo5_3.cpp

--- ejemplo5_3.cpp
+++ ejemplo5_3.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
    char car;
-   bool digito_leido=false;
+   bool digito_leido = false;
    while (!digito_leido)
    {
        cout << "Introduzca un caracter:"; 
        cin >> car;
-       digito_leido = (('0'<=car)&&(car<='9'));
+       digito_leido = (('0' <= car) && (car <= '9'));
    }
    return 0;
 }
